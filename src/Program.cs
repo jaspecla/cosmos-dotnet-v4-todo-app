504bd3b9d985eaac36600cdd2ddd742d737e4637
Jason Specland <jaspecla@microsoft.com>
2021-03-01 12:15:02 -0500

    Cosmos key now comes from Key Vault

--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,29 @@
 ﻿namespace todo
 {
-    using Microsoft.AspNetCore;
-    using Microsoft.AspNetCore.Hosting;
+  using Azure.Extensions.AspNetCore.Configuration.Secrets;
+  using Azure.Identity;
+  using Azure.Security.KeyVault.Secrets;
+  using Microsoft.AspNetCore;
+  using Microsoft.AspNetCore.Hosting;
+  using Microsoft.Extensions.Configuration;
+  using System;
 
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((context, config) =>
+               {
+                 var builtConfig = config.Build();
+                 var secretClient = new SecretClient(new Uri($"{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+                   new DefaultAzureCredential());
+                 config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+               })
+            .UseStartup<Startup>();
+  }
 }
