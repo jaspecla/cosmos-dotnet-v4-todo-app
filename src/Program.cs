ff0a68feb7ef79609684d93955942b2c26f4475c
Jason Specland <jaspecla@microsoft.com>
2021-04-13 20:58:36 -0400

    removed keyvault integration

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,7 @@
 ﻿namespace todo
 {
-  using Azure.Extensions.AspNetCore.Configuration.Secrets;
-  using Azure.Identity;
-  using Azure.Security.KeyVault.Secrets;
   using Microsoft.AspNetCore;
   using Microsoft.AspNetCore.Hosting;
-  using Microsoft.Extensions.Configuration;
-  using System;
 
   public class Program
   {
@@ -17,16 +12,6 @@
 
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
-               .ConfigureAppConfiguration((context, config) =>
-               {
-                 if (context.HostingEnvironment.IsProduction())
-                 {
-                   var builtConfig = config.Build();
-                   var secretClient = new SecretClient(new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
-                     new DefaultAzureCredential());
-                   config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
-                 }
-               })
             .UseStartup<Startup>();
   }
 }
